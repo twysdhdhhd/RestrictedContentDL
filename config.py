aba5e84ba578fc2f12fc7b8b40b2f14151274103
twysdhdhhd <plnqkg@ubyl.lol>
2025-10-30 20:09:51 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,31 +1,25 @@
-# Copyright (C) @TheSmartBisnu
-# Channel: https://t.me/itsSmartDev
-
-from os import getenv
+import os
 from time import time
 from dotenv import load_dotenv
 
-try:
-    load_dotenv("config.env")
-except:
-    pass
+load_dotenv("config.env", override=False)
 
-    if not getenv("BOT_TOKEN") or not getenv("BOT_TOKEN").count(":") == 1:
-        print("Error: BOT_TOKEN must be in format '123456:abcdefghijklmnopqrstuvwxyz'")
-        exit(1)
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+SESSION_STRING = os.getenv("SESSION_STRING")
 
-    if (
-        not getenv("SESSION_STRING")
-        or getenv("SESSION_STRING") == "xxxxxxxxxxxxxxxxxxxxxxx"
-    ):
-        print("Error: SESSION_STRING must be set with a valid string")
-        exit(1)
+if not BOT_TOKEN or ":" not in BOT_TOKEN:
+    print("Error: BOT_TOKEN must be in format '123456:abcdefghijklmnopqrstuvwxyz'")
+    exit(1)
 
+if not SESSION_STRING or SESSION_STRING.startswith("x"):
+    print("Error: SESSION_STRING must be a valid Pyrogram session string")
+    exit(1)
 
-# Pyrogram setup
-class PyroConf(object):
-    API_ID = int(getenv("API_ID", "6"))
-    API_HASH = getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-    BOT_TOKEN = getenv("BOT_TOKEN")
-    SESSION_STRING = getenv("SESSION_STRING")
+class PyroConf:
+    API_ID = int(API_ID)
+    API_HASH = API_HASH
+    BOT_TOKEN = BOT_TOKEN
+    SESSION_STRING = SESSION_STRING
     BOT_START_TIME = time()
